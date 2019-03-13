660779a9246e5c68e1eef846b33a1da52186730b
dustin-boettcher <43124981+dustin-boettcher@users.noreply.github.com>
2019-03-13 10:35:22 -0500

    Update StepperMotorController.c

--- StepperMotorController.c
+++ StepperMotorController.c
@@ -1,8 +1,8 @@
 // StepperMotorController.c starter file EE319K Lab 5
 // Runs on TM4C123
 // Finite state machine to operate a stepper motor.  
-// Jonathan Valvano
-// January 18, 2019
+// Dustin Boettcher, Chris Clark
+// March 13, 2019
 
 // Hardware connections (External: two input buttons and four outputs to stepper motor)
 //  PA5 is Wash input  (1 means pressed, 0 means not pressed)
@@ -24,8 +24,6 @@ struct State {
 	uint16_t next[4];
 };
 typedef const struct State STyp;
-
-#define initial 0
 	#define home 1
 	#define wipe 2
 	#define wash 19
@@ -43,7 +41,7 @@ STyp FSM[40] = {
 	{2, 0, 50, {8, 8, 8, 8}}, //7
 	{4, 0, 50, {9, 9, 9, 9}}, //8
 	{8, 0, 50, {10, 10, 10, 10}}, //9
-	{16, 0, 50, {11, 11, 11, 11}}, //10
+	{16, 0, 50, {36, 36, 36, 36}}, //10
 	{8, 0, 50, {12, 12, 12, 12}}, //11
 	{4, 0, 50, {13, 13, 13, 13}}, //12
 	{2, 0, 50, {14, 14, 14, 14}}, //13
@@ -62,7 +60,7 @@ STyp FSM[40] = {
 	{2, 0, 50, {25, 25, 25, 25}}, //24
 	{4, 1, 50, {26, 26, 26, 26}}, //25
 	{8, 0, 50, {27, 27, 27, 27}}, //26
-	{16, 1, 50, {28, 28, 28, 28}}, //27
+	{16, 1, 50, {38, 38, 38, 38}}, //27
 	{8, 0, 50, {29, 29, 29, 29}}, //28
 	{4, 1, 50, {30, 30, 30, 30}}, //29
 	{2, 0, 50, {31, 31, 31, 31}}, //30
@@ -72,34 +70,32 @@ STyp FSM[40] = {
 	{4, 0, 50, {35, 35, 35, 35}}, //34
 	{2, 1, 50, {home, home, home, home}}, //35
 	
+	{1, 0, 50, {37, 37, 37, 37}}, //36
+	{16, 0, 50, {11, 11, 11, 11}}, //37
+	
+	{1, 0, 50, {39, 39, 39, 39}}, //38
+	{16, 1, 50, {28, 28, 28, 28}} //39
+	
 };
-	
-	
-	
-	
-	
-	
-	
 void EnableInterrupts(void);
 // edit the following only if you need to move pins from PA4, PE3-0      
 // logic analyzer on the real board
 #define PA4       (*((volatile unsigned long *)0x40004040))
 #define PE50      (*((volatile unsigned long *)0x400240FC))
-uint16_t input;
 void SendDataToLogicAnalyzer(void){
   UART0_DR_R = 0x80|(PA4<<2)|PE50;
 }
 
 int main(void){ 
 	
-	uint16_t S=0;
+	uint16_t State=0;
+	uint16_t input;
   TExaS_Init(&SendDataToLogicAnalyzer);    // activate logic analyzer and set system clock to 80 MHz
   SysTick_Init();   
   
 		
 	SYSCTL_RCGCGPIO_R|=0x33;
-	while((SYSCTL_RCGCGPIO_R&0x33)!=0x33){
-	}
+	while((SYSCTL_RCGCGPIO_R&0x33)!=0x33){}
 	
 	GPIO_PORTA_DEN_R|=0x30;
 	GPIO_PORTA_DIR_R&=0xCF;
@@ -110,22 +106,13 @@ int main(void){
 	GPIO_PORTE_DEN_R|=0x3F;
 	GPIO_PORTE_DIR_R|=0x3F;
 	
-	//GPIO_PORTF_DEN_R|=0x1;
-	//GPIO_PORTF_DIR_R|=0x1;
-	
-
-	
   EnableInterrupts();
 	
 
   while(1){
-		GPIO_PORTE_DATA_R = FSM[S].Output; //Output
-		GPIO_PORTE_DATA_R += (FSM[S].flash<<5);
-		
-		SysTick_Wait1ms(FSM[S].Time); //wait
-		
-		input = ((GPIO_PORTA_DATA_R & 0x30)>>4); // input should be the two buttons
-		
-    S=FSM[S].next[input];// next	
+		GPIO_PORTE_DATA_R = (GPIO_PORTE_DATA_R & 0xC0) + FSM[State].Output + (FSM[State].flash<<5); //adds output
+		SysTick_Wait1ms(FSM[State].Time); //wait
+		input = ((GPIO_PORTA_DATA_R & 0x30)>>4); // input is the two buttons PA4-5
+		State = FSM[State].next[input];// next	
   }
 }
